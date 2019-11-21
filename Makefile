cb04efddecb70c1936320a705053d8a55c1f034a
Josh Chorlton <jchorlton@gmail.com>
2019-11-20 20:36:48 -0800

    generation works on button click

--- Makefile
+++ Makefile
@@ -1,6 +1,21 @@
 UID=$(shell id -u)
 GID=$(shell id -g)
 
+dev-serve:
+	docker run -it --rm \
+		-v $(PWD):/tlscerts:ro \
+		-w /tlscerts \
+		-p 8080:8080 \
+		python:3.8 \
+		python -m http.server 8080
+
+prettier:
+	docker run -it --rm \
+		-v $(PWD):/tlscerts \
+		-u $(UID):$(GID) \
+		node:13.1 \
+		sh -c "mkdir ~/.npm-global && npm config set prefix '~/.npm-global' && npm install -g prettier && ~/.npm-global/bin/prettier --write /tlscerts/index.html"
+
 wasm:
 	docker run -it --rm \
 		-v $(PWD):/tlscerts \
--- main.go
+++ main.go
@@ -2,27 +2,58 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"strings"
+	"syscall/js"
 
 	"github.com/cloudflare/cfssl/csr"
 	"github.com/cloudflare/cfssl/initca"
 	"github.com/cloudflare/cfssl/log"
 )
 
-func main() {
-	flag.Parse()
+// CertBundle just packages up a public cert and private key together
+type CertBundle struct {
+	Public  []byte
+	Private []byte
+}
 
+func generateCACert(commonName string, hosts []string) (CertBundle, error) {
 	req := csr.CertificateRequest{
-		CN:         "CA",
-		Hosts:      []string{"tlscerts.dev"},
+		CN:         commonName,
+		Hosts:      hosts,
 		KeyRequest: csr.NewKeyRequest(),
 	}
 
-	cert, csrPEM, key, err := initca.New(&req)
+	cert, _, key, err := initca.New(&req)
 	if err != nil {
-		log.Fatalf("initca.New(...): %s", err)
+		return CertBundle{}, fmt.Errorf("initca.New(...): %w", err)
 	}
 
-	log.Infof("cert: %s", cert)
-	log.Infof("csr: %s", csrPEM)
-	log.Infof("key: %s", key)
+	return CertBundle{Public: cert, Private: key}, nil
+}
+
+func run(this js.Value, inputs []js.Value) interface{} {
+	caCommonName := js.Global().Get("document").Call("getElementById", "root_common_name").Get("value").String()
+	caHostsJoined := js.Global().Get("document").Call("getElementById", "root_hosts").Get("value").String()
+	caHosts := strings.Split(caHostsJoined, ",")
+
+	bundle, err := generateCACert(caCommonName, caHosts)
+	if err != nil {
+		log.Errorf("generateCACert(%s, %v): %s", caCommonName, caHosts, err)
+	}
+
+	log.Infof("public: %s", bundle.Public)
+	log.Infof("private: %s", bundle.Private)
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	js.Global().Set("run", js.FuncOf(run))
+
+	done := make(chan bool)
+
+	<-done
 }
