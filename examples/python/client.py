cb36b3855cf34d87e700737713924454c3df3f02
Josh Chorlton <jchorlton@gmail.com>
2019-11-21 19:00:10 -0800

    adding python integration test

---
+++ examples/python/client.py
@@ -0,0 +1,6 @@
+import requests
+
+resp = requests.get('https://server/', cert=('./client.pem', './client-key.pem'), verify='./ca.pem')
+if resp.status_code != 200:
+    print('expected status_code 200 but got {}'.format(resp.status_code))
+    exit(1)
---
+++ examples/python/server.py
@@ -0,0 +1,12 @@
+import ssl
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hi!'
+
+ctx = ssl.create_default_context(capath='./ca.pem')
+ctx.load_cert_chain(certfile='./server.pem', keyfile='./server-key.pem')
+app.run(debug=False, ssl_context=ctx)
